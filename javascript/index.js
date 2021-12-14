f609670125026900b166949478c212f2e38a8c77
Kamilla <kamillakkngmail.com>
2021-12-14 12:36:47 +0100

    changes to index

--- javascript/index.js
+++ javascript/index.js
@@ -1,16 +1,25 @@
-//kode til burger menu mobil version
+// //kode til burger menu mobil version
 
-const btn = document.querySelector('.toggle-btn');
-const nav = document.querySelector('nav');
+// const btn = document.querySelector('.toggle-btn');
+// const nav = document.querySelector('nav');
 
-function toggleMenu() {
-  nav.classList.toggle('shown');
-  const menuShown = nav.classList.contains('shown');
-  if (menuShown) {
-    btn.textContent = 'Luk';
+// function toggleMenu() {
+//   nav.classList.toggle('shown');
+//   const menuShown = nav.classList.contains('shown');
+//   if (menuShown) {
+//     btn.textContent = 'Luk';
+//   } else {
+//     btn.textContent = 'Menu';
+//   }
+// }
+// btn.addEventListener('click', toggleMenu);
+// // slut
+
+function myFunction() {
+  var x = document.getElementById('myTopnav');
+  if (x.className === 'topnav') {
+    x.className += ' responsive';
   } else {
-    btn.textContent = 'Menu';
+    x.className = 'topnav';
   }
 }
-btn.addEventListener('click', toggleMenu);
-// slut
